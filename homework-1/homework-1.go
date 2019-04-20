84539fbac909b4865cef8fb80ca5aa9a2994b8e7
Khamenev <khamenev@me.com>
2019-04-21 00:43:02 +0300

    Упражнения 1 и 2 сделаны

--- homework-1/homework-1.go
+++ homework-1/homework-1.go
@@ -15,8 +15,14 @@ func main() {
 	fmt.Println("Your amount in USD is", converter(amountRur), "$")
 
 	// Находим площадь, периметр и гипотенузу треугольника
-	fmt.Println("Please enter your amount in RUR")
-	fmt.Scanln(&amountRur)
+	var a float64 // катет 1
+	var b float64 // катет 2
+	fmt.Println("Please enter the legs of a right triangle")
+	fmt.Scanln(&a, &b)
+	var c = triangleHypotenuse(a, b) // гипотенуза
+	fmt.Println("The area of a right triangle:", triangleSqrt(a, b))
+	fmt.Println("The perimeter of a right triangle:", trianglePerimeter(a, b, c))
+	fmt.Println(c)
 }
 
 // Написать программу для конвертации рублей в доллары. Программа запрашивает сумму в
@@ -24,8 +30,23 @@ func main() {
 
 func converter(amountRur float64) float64 {
 	const currencyUsd float64 = 65.5
-	return math.Round(amountRur / currencyUsd) //округляем до целого числа
+	return (amountRur / currencyUsd)
 }
 
 //Даны катеты прямоугольного треугольника. Найти его площадь, периметр и гипотенузу.
 //Используйте тип данных float64 и функции из пакета math.
+
+//Функция по вычислению площади
+func triangleSqrt(a, b float64) float64 {
+	return a * b * 0.5
+}
+
+//Функция по вычислению гипотенузы
+func triangleHypotenuse(a, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
+
+//Функция по вычислению периметра
+func trianglePerimeter(a, b, c float64) float64 {
+	return a + b + c
+}
